cae746a23b55823afc14bded271bd8bd5fdb51ee
Satoshi Ogawa <osatoshi.11@gmail.com>
2020-11-03 15:40:57 +0900

    feat: プログラムをQで終了できるようになった

--- src/main.py
+++ src/main.py
@@ -1,10 +1,23 @@
 import logging
 import threading
 import time
+import keyboard
 
 logging.basicConfig(level=logging.DEBUG, format='%(threadName)s: %(message)s')
 
 stop = False  # スレッドの終了用
+
+
+def worker_log(event_log):
+    """ログ記録のためのworker
+    """
+    while not stop:
+        # event.set が実行されるまで待機
+        event_log.wait()
+        event_log.clear()
+        logging.debug('start')
+        time.sleep(3)
+        logging.debug('end')
 
 
 def worker_voice(event_voice):
@@ -31,34 +44,38 @@ def worker_notification(event_notification):
         logging.debug('end')
 
 
-def worker_main(event_voice, event_notification):
+def worker_main(event_log, event_voice, event_notification):
     """処理の中心となるworker
     """
     global stop
     logging.debug('start')
-    time.sleep(5)
-    # event.waitにしたスレッドを実行
-    event_voice.set()
-    time.sleep(5)
-    event_notification.set()
-    time.sleep(5)
-    event_voice.set()
-    time.sleep(5)
-    logging.debug('end')
-    stop = True
-    exit()
+    print(stop)
+    while not stop:
+        if keyboard.is_pressed("q"):
+            logging.debug("q is pressed, program end")
+            exit()
+            stop = True
+        time.sleep(1)
+        logging.debug("1 sec")
 
 
 if __name__ == '__main__':
+    event_log = threading.Event()
     event_voice = threading.Event()
     event_notification = threading.Event()
 
     thread_main = threading.Thread(
         name="thread_main",
         target=worker_main,
-        args=(event_voice, event_notification, ))
+        args=(event_log, event_voice, event_notification, ))
 
     # メインスレッド以外はすべてデーモン化
+    thread_log = threading.Thread(
+        name="thread_log",
+        target=worker_log,
+        args=(event_log,))
+    thread_log.setDaemon(True)
+
     thread_voice = threading.Thread(
         name="thread_voice",
         target=worker_voice,
@@ -74,3 +91,4 @@ if __name__ == '__main__':
     thread_main.start()
     thread_voice.start()
     thread_notification.start()
+    thread_log.start()
