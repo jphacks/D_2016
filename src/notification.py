1c11435b875929fd72b889594732545bf205fb83
Satoshi Ogawa <osatoshi.11@gmail.com>
2020-11-03 22:22:38 +0900

    feat: 関数を追加

--- src/notification.py
+++ src/notification.py
@@ -1,6 +1,12 @@
 import os
 
-path = "notification.ps1"
 
-os.system('powershell -Command' + ' ' +\
-          'powershell -ExecutionPolicy RemoteSigned .\\'+path)
+def notify():
+    path = "notification.ps1"
+
+    os.system('powershell -Command' + ' ' +
+              'powershell -ExecutionPolicy RemoteSigned .\\'+path)
+
+
+if __name__ == "__main__":
+    notify()
