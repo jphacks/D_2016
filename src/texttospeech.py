b599f1f7d2ba6278edb2b6787906589e88369664
Satoshi Ogawa <osatoshi.11@gmail.com>
2020-11-04 20:51:23 +0900

    refactor: 音声をmedia配下にまとめる

--- src/texttospeech.py
+++ src/texttospeech.py
@@ -1,9 +1,11 @@
 import winsound
 
+
 def sound():
-    with open("test.wav", "rb") as f:
-        data = f.read()        
+    with open("media/test.wav", "rb") as f:
+        data = f.read()
     winsound.PlaySound(data, winsound.SND_MEMORY)
+
 
 if __name__ == '__main__':
     sound()
