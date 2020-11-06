158a7080cb723b08bf3c95af056d6f6427b347b0
Satoshi Ogawa <osatoshi.11@gmail.com>
2020-11-06 15:48:15 +0900

    feat: 作業時間に応じて応援する

--- src/text_generator.py
+++ src/text_generator.py
@@ -1,20 +1,26 @@
 import count_docx
 import sound
+import activity_log
+
+
 def generate_text(event) -> str:
-    
+
     if event == "start":
         txt = "今日もいちにち頑張りましょう!"
 
     if event == "terminate":
         txt = count_docx.count_docx("test.docx")
         txt = str(txt) + "文字記載おつかれさまです"
-    
+
     if event == "cheer":
-        txt = "お疲れ様です。ちょっと休憩しましょうか?"
-        
+        working_time_second = activity_log.get_working_time_on_current_window(
+            interval=2)
+        working_time_hour = working_time_second // 3600
+        txt = str(working_time_hour) + "時間の作業お疲れ様です。ちょっと休憩しましょうか?"
+
     if event == "praise":
         txt = "よく頑張っていますね!"
-    
+
     if event == None:
         txt = "eventが指定されていません"
 
@@ -22,9 +28,9 @@ def generate_text(event) -> str:
 
 # TODO たまに名前で読んでくれる?
 
+
 if __name__ == '__main__':
     event = "cheer"
     txt = generate_text(event)
     print(txt)
     sound.play_sound(txt)
-    
