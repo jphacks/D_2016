57430bcd13df7dca09b800a0f6728034735291e5
jamjamjam <dubian886@email.com>
2020-11-06 17:19:34 +0900

    delete:使わないフォルダを削除

--- src/texttospeech/gtts.py
+++
@@ -1,5 +0,0 @@
-# Windowsのpipが死んでいたので動かせなかったがMacでは動いているので大丈夫なはず
-
-from gtts import gTTS
-hoge = gTTS("Hello World")
-hoge.save("sample.mp3")
--- src/texttospeech/texttospeech.py
+++
@@ -1,6 +0,0 @@
-import winsound
-
-with open("test.wav", "rb") as f:
-    data = f.read()
-    
-winsound.PlaySound(data, winsound.SND_MEMORY)
