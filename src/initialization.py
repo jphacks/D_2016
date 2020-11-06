da0ffc82f3af94ccec53239123a5de46eda5cf9a
Satoshi Ogawa <osatoshi.11@gmail.com>
2020-11-06 15:23:53 +0900

    fix: 初期化時にsrc 配下にもbatファイルを作成

--- src/initialization.py
+++ src/initialization.py
@@ -12,23 +12,31 @@ def add_to_startup(file_path: str):
     USER_NAME = getpass.getuser()
     assert os.path.exists(file_path), "指定されたfile_pathがありません: " + file_path
     abs_file_path = os.path.abspath(file_path)  # 絶対パス
-    src_path = os.path.dirname(abs_file_path)
     bat_path = r'C:\Users\{}\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup'.format(
         USER_NAME)
     # print(bat_path)
+
+    # スタートアップ/配下 と src/配下 にbatを作成
     with open(bat_path + '\\'"komati.bat", "w+") as bat_file:
         text = "cd " + os.path.dirname(abs_file_path) + "\n"
         text += 'python {}'.format(file_path)
         bat_file.write(text)
+    print(file_path + " を実行する bat ファイルをスタートアップに登録しました")
+
+    with open("komati.bat", "w+") as bat_file:
+        text = "cd " + os.path.dirname(abs_file_path) + "\n"
+        text += 'python {}'.format(file_path)
+        bat_file.write(text)
+    print(file_path + " を実行する bat ファイルを src配下に作成しました")
 
     # 黒画面を出さない
+    # src_path = os.path.dirname(abs_file_path)
     # with open(bat_path + '\\' + "komati_no_window.vbs", "w+") as vbs_file:
     #     text = 'Set ws = CreateObject("Wscript.Shell")' + "\n"
     #     text += 'ws.CurrentDirectory = "{}"'.format(src_path) + "\n"
     #     text += 'ws.run "cmd /c /komati.bat", vbhid' + "\n"
     #     text += ""
     #     vbs_file.write(text)
-    print(file_path + " をスタートアップに登録しました")
 
 
 if __name__ == "__main__":
